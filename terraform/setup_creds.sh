606bae44ee5d8a084b71f888d9f82f7bb878c2c7
Jens Barthel <j.barthel@fielmann.com>
2021-03-13 17:33:31 +0100

    Remove obsolete provisioning code

--- terraform/setup_creds.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-echo "export DO_PAT=$(cat ~/.terraform.d/do_apikey)"
-echo "export DO_SSH_FINGERPRINT=$(ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | sed 's/^MD5://')"
-echo "export TF_LOG=1"
